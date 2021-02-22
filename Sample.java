6d42ea34935448e0e17ca171957c84f39fdcb670
Prabhurbvn <prabhurbvn@gmail.com>
2021-02-22 12:25:23 +0530

    Update add method

--- Sample.java
+++ Sample.java
@@ -1,5 +1,15 @@
 public class sample
 {
+
+public void add()
+{
+  int a=5,b=10,c;
+
+  c=a+b;
+ 
+ System.out.println(c);
+}
+ 
  public static void main(String[] args)
  {
   System.out.println("Hi");
